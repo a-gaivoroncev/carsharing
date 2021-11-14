36b70a1d7334fd6eb7ae9071ba5bfd54bba0326a
AlexanderGaivorontsev <festive.muxaujl@gmail.com>
2021-11-15 01:31:48 +0300

    add database config

---
+++ src/modules/database/database.service.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ModuleRef } from "@nestjs/core";
+import { Pool, QueryResult } from "pg";
+import { DATABASE_POOL } from "src/constants";
+
+@Injectable()
+export class DatabaseService {
+  private readonly logger = new Logger(DatabaseService.name);
+
+  constructor(
+    // @Inject('DATABASE_POOL') private poolRepository: Pool,
+    // private readonly configServ: typeof ConfigService
+   private readonly moduleRef: ModuleRef
+   ) {}
+   
+
+  executeQuery(queryText: string, values: any[] = []): Promise<any[]> {
+    this.logger.debug(`Executing query: ${queryText} (${values})`);
+    
+    const pool = this.moduleRef.get(DATABASE_POOL) as Pool;
+    return pool.query(queryText, values).then((result: QueryResult) => {
+      this.logger.debug(`Executed query, result size ${result.rows.length}`);
+      return result.rows;
+    });
+  }
+}
