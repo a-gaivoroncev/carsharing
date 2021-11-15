ad33c96ec7c9cdc29e199b3a73ccef631858e478
AlexanderGaivorontsev <festive.muxaujl@gmail.com>
2021-11-15 13:41:14 +0300

    Add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { SeedService } from './modules/seed/seed.service';
 
@@ -9,6 +9,14 @@ async function bootstrap() {
   app.enableShutdownHooks();
   const seedService = app.get(SeedService);
   await seedService.seed();
+  const config = new DocumentBuilder()
+    .setTitle('Carsharing API')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
